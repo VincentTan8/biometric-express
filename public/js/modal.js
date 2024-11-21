4df70f526f700325c654a9f40095a327ba50e8a4
VincentTan8 <vincent.tan@wetalk.com>
2024-11-21 10:41:08 +0800

    refactor: modified modal js to accomodate multiple modals, added updateModal button logic

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,34 +1,41 @@
-//Modal that appears when update is finished
-const modal = document.getElementById('updateModal')
-// Get the <span> element that closes the modal
-const closeSpan = document.querySelector('.close')
+//for all modals
+document.querySelectorAll('.modal').forEach(modal => {
+    // Get the <span> element that closes the modal
+    const closeSpan = modal.querySelector('.close')
 
-// Close modal when x icon/span is clicked
-closeSpan.addEventListener('click', () => {
-    closeModal()
-})
-// Close modal when clicking on the modal background (not the modal content)
-window.addEventListener('click', (event) => {
-    if (event.target === modal) {
-        closeModal()
-    }
-})
-// Close modal when pressing the "Escape" key
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-        closeModal()
-    }
+    // Close modal when x icon/span is clicked
+    closeSpan.addEventListener('click', () => {
+        closeModal(modal)
+    })
+    // Close modal when clicking on the modal background (not the modal content)
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal(modal)
+        }
+    })
+    // Close modal when pressing the "Escape" key
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            closeModal(modal)
+        }
+    })
 })
 
-function openModal() {
-    modal.style.display = 'block'
-}
-
-function closeModal() {
+function closeModal(modal) {
     modal.style.display = 'none'
 }
 
-function closeTab() {
-    window.close() //only works if window was opened through javascript
-    closeModal()
+//for the modal in index.html
+const updateOK = document.getElementById('updateOK')
+if(updateOK) {
+    updateOK.addEventListener('click', () => {
+        window.close() //only works if window was opened through javascript
+        closeModal(document.getElementById('updateModal'))
+    })
+}
+const updateCancel = document.getElementById('updateCancel')
+if(updateCancel) {
+    updateCancel.addEventListener('click', () => {
+        closeModal(document.getElementById('updateModal'))
+    })
 }
