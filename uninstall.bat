60e51fadb58f96ec8c6f250c7d9b967ba3cda89f
VincentTan8 <vincent.tan@wetalk.com>
2025-01-03 14:12:09 +0800

    fix: fixed node path

--- uninstall.bat
+++ uninstall.bat
@@ -1,8 +1,8 @@
 @echo off
-set NODE_DIR=%~dp0node-v22.11.0-win-x64
+set NODE_DIR=%~dp0node-v22.11.0-win-x64\node.exe
 
 :: Uninstall the service
-%NODE_PATH% uninstallService.js
+%NODE_DIR% uninstallService.js
 
 echo Uninstall finished!
 pause
