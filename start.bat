76c5bf662c687aca37667af909b02fb900ee5c2a
VincentTan8 <vincent.tan@wetalk.com>
2025-01-03 11:41:54 +0800

    fix: fixed node directory name

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
-set NODE_DIR=%~dp0node-v22.11.0-win-x64
+:: set NODE_DIR=%~dp0node-v22.11.0-win-x64
+set NODE_DIR=%APPDATA%\biometric-express-main\node-v22.11.0-win-x64
 set APP_DIR=%APPDATA%
 :: start /min "Biometric" "%NODE_DIR%\node.exe" app.js
 :: start http://localhost:3000
@@ -13,7 +14,7 @@ IF NOT EXIST "%APP_DIR%" (
 powershell -Command "Expand-Archive -Path biometric-express-main.zip -DestinationPath %APP_DIR% -Force"
 
 :: Register and run the service
-%NODE_PATH% registerService.js
+%NODE_DIR% registerService.js
 
 echo Installation completed!
 pause
