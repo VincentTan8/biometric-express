4faf6b8774a400eb42a8def0e2db76ede3d03994
VincentTan8 <vincent.tan@wetalk.com>
2025-01-03 14:10:49 +0800

    fix: changed app js path to current working directory

--- registerService.js
+++ registerService.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const Service = require('node-windows').Service
 
-const appDataPath = path.join(process.env.APPDATA, 'biometric-express-main', 'app.js')
+const appPath = path.join(process.cwd(), 'app.js')
 
 const svc = new Service({
   name: 'Biometrics', 
   description: 'Node app running to maintain connection to biometric device', 
-  script: appDataPath, 
+  script: appPath, 
   env: [{
     name: 'NODE_ENV',
     value: 'production' 
