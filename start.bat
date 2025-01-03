9f230379fcdb81b7a318913876e059ba34ef265a
VincentTan8 <vincent.tan@wetalk.com>
2025-01-03 11:20:25 +0800

    refactor: modified start batch file to registering node app as a service

--- start.bat
+++ start.bat
@@ -1,4 +1,19 @@
 @echo off
 set NODE_DIR=%~dp0node-v22.11.0-win-x64
-start /min "Biometric" "%NODE_DIR%\node.exe" app.js
-start http://localhost:3000
+set APP_DIR=%APPDATA%
+:: start /min "Biometric" "%NODE_DIR%\node.exe" app.js
+:: start http://localhost:3000
+
+:: Create the application directory
+IF NOT EXIST "%APP_DIR%" (
+    mkdir "%APP_DIR%"
+)
+
+:: Extract application files
+powershell -Command "Expand-Archive -Path biometric-express-main.zip -DestinationPath %APP_DIR% -Force"
+
+:: Register and run the service
+%NODE_PATH% registerService.js
+
+echo Installation completed!
+pause
