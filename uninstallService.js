204798baab18af0cfddd97adccf312cd7c793450
VincentTan8 <vincent.tan@wetalk.com>
2025-01-03 15:31:26 +0800

    fix: added path to service

--- uninstallService.js
+++ uninstallService.js
@@ -1,7 +1,11 @@
+const path = require('path')
 const Service = require('node-windows').Service;
 
+const appPath = path.join(process.cwd(), 'app.js')
+
 const svc = new Service({
-  name: 'Biometrics'
+  name: 'Biometrics',
+  script: appPath,
 });
 
 svc.on('uninstall', () => {
